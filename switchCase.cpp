7cc8dd0e0e120efd6e74474098427da1a71a881a
akmalropip <akmalropip@gmail.com>
2026-03-30 18:54:11 +0700

    menambahkan prosedur di file switchCase

--- switchCase.cpp
+++ switchCase.cpp
@@ -7,3 +7,35 @@ string namaJurusan;
 string statusKuota;
 
 //prosedur input
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << end1;
+    cout << "----------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodeJurusan;
+}
+
+//prosedur untuk pengecekan kondisi
+void pilihanJurusan() {
+    switch (kodeJurusan) {
+        case 1:
+        namaJurusan = "Teknologi Informasi";
+        statusKuota = "Tersedia (15 kursi)";
+        break;
+         case 2:
+        namaJurusan = "Teknik Elektro";
+        statusKuota = "Kuota Penuh!";
+        break;
+         case 3:
+        namaJurusan = "Sistem Informasi";
+        statusKuota = "Tersedia (5 kursi)";
+        break;
+         default:
+        namaJurusan = "Tidak Diketahui";
+        statusKuota = "Error: Kode Tidak Valid";
+    }
+}
+
+//prosedur
