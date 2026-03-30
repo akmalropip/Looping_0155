d7e94344cb13e28cdc9180105c0b37d786002bbb
akmalropip <akmalropip@gmail.com>
2026-03-30 16:30:48 +0700

    perulangan dengan DO...WHILE di file looping

--- looping.cpp
+++ looping.cpp
@@ -16,4 +16,16 @@ int main(){
     srand(time(0));
 
     //perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }
+
+    cout<<"Bilangan acak while yang terakhir ="<<x <<endl;
+    cout<<endl;
+
+    //perulangan dengan DO...WHILE
 }
